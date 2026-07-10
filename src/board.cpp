ab16507e0217d8f27aabf12ca6352990029c9c15
martxrrr <wainainam008@gmail.com>
2026-07-10 22:11:05 +0300

    piece rendering on the board

--- src/board.cpp
+++ src/board.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <cstdint>
 #include <typeinfo>
+#include <initializer_list>
 
 void  Click(sf::RenderWindow &window, const sf::Event &event){
     if(const auto* mouseClick = event.getIf<sf::Event::MouseButtonPressed>()){
@@ -18,6 +19,80 @@ void  Click(sf::RenderWindow &window, const sf::Event &event){
     }
 }
 
+struct position{
+        sf::Vector2f A1 = { 81.f, 597.f };
+        sf::Vector2f A2 = { 81.f, 523.f };
+        sf::Vector2f A3 = { 81.f, 450.f };
+        sf::Vector2f A4 = { 81.f, 377.f };
+        sf::Vector2f A5 = { 81.f, 304.f };
+        sf::Vector2f A6 = { 81.f, 231.f };
+        sf::Vector2f A7 = { 81.f, 153.f };
+        sf::Vector2f A8 = { 81.f, 80.f  };
+
+        sf::Vector2f B1 = { 154.f, 597.f };
+        sf::Vector2f B2 = { 154.f, 523.f };
+        sf::Vector2f B3 = { 154.f, 450.f };
+        sf::Vector2f B4 = { 154.f, 377.f };
+        sf::Vector2f B5 = { 154.f, 304.f };
+        sf::Vector2f B6 = { 154.f, 231.f };
+        sf::Vector2f B7 = { 154.f, 153.f };
+        sf::Vector2f B8 = { 154.f, 80.f  };
+
+        sf::Vector2f C1 = { 227.f, 597.f };
+        sf::Vector2f C2 = { 227.f, 523.f };
+        sf::Vector2f C3 = { 227.f, 450.f };
+        sf::Vector2f C4 = { 227.f, 377.f };
+        sf::Vector2f C5 = { 227.f, 304.f };
+        sf::Vector2f C6 = { 227.f, 231.f };
+        sf::Vector2f C7 = { 227.f, 153.f };
+        sf::Vector2f C8 = { 227.f, 80.f  };
+
+        sf::Vector2f D1 = { 300.f, 600.f };
+        sf::Vector2f D2 = { 300.f, 523.f };
+        sf::Vector2f D3 = { 300.f, 450.f };
+        sf::Vector2f D4 = { 300.f, 377.f };
+        sf::Vector2f D5 = { 300.f, 304.f };
+        sf::Vector2f D6 = { 300.f, 231.f };
+        sf::Vector2f D7 = { 300.f, 153.f };
+        sf::Vector2f D8 = { 300.f, 80.f  };
+
+        sf::Vector2f E1 = { 374.f, 597.f };
+        sf::Vector2f E2 = { 374.f, 523.f };
+        sf::Vector2f E3 = { 374.f, 450.f };
+        sf::Vector2f E4 = { 374.f, 377.f };
+        sf::Vector2f E5 = { 374.f, 304.f };
+        sf::Vector2f E6 = { 374.f, 231.f };
+        sf::Vector2f E7 = { 374.f, 153.f };
+        sf::Vector2f E8 = { 374.f, 77.f  };
+
+        sf::Vector2f F1 = { 447.f, 597.f };
+        sf::Vector2f F2 = { 447.f, 523.f };
+        sf::Vector2f F3 = { 447.f, 450.f };
+        sf::Vector2f F4 = { 447.f, 377.f };
+        sf::Vector2f F5 = { 447.f, 304.f };
+        sf::Vector2f F6 = { 447.f, 231.f };
+        sf::Vector2f F7 = { 447.f, 153.f };
+        sf::Vector2f F8 = { 451.f, 80.f  };
+
+        sf::Vector2f G1 = { 520.f, 597.f };
+        sf::Vector2f G2 = { 520.f, 523.f };
+        sf::Vector2f G3 = { 520.f, 450.f };
+        sf::Vector2f G4 = { 520.f, 377.f };
+        sf::Vector2f G5 = { 520.f, 304.f };
+        sf::Vector2f G6 = { 520.f, 231.f };
+        sf::Vector2f G7 = { 520.f, 153.f };
+        sf::Vector2f G8 = { 524.f, 80.f  };
+
+        sf::Vector2f H1 = { 593.f, 597.f };
+        sf::Vector2f H2 = { 593.f, 523.f };
+        sf::Vector2f H3 = { 593.f, 450.f };
+        sf::Vector2f H4 = { 593.f, 377.f };
+        sf::Vector2f H5 = { 593.f, 304.f };
+        sf::Vector2f H6 = { 593.f, 231.f };
+        sf::Vector2f H7 = { 593.f, 153.f };
+        sf::Vector2f H8 = { 597.f, 80.f   };
+};
+
 class Pieces{
         public:
 
@@ -28,7 +103,8 @@ class Pieces{
                                 std::cout << "Error loading the image file! " << "\n";
                         }
                         sf::Sprite kingB(king);
-                        kingB.setPosition({374.f, 77.f});
+                        sf::Vector2f pos = {374.f, 77.f};
+                        kingB.setPosition(pos);
 
                         window.draw(kingB);
                 }else if(color == 1){
