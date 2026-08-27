506496a4a1c467debe9cb4b3d237b9b390624c47
martxrrr <wainainam008@gmail.com>
2026-08-27 21:37:00 +0300

    updating bitboards

--- src/bitboard.cpp
+++ src/bitboard.cpp
@@ -6,19 +6,16 @@
 
 //an enum that reqpresents each square number on the board
 enum Square : int {
-        SQ_A1 = 0,  SQ_A2 = 1,  SQ_A3 = 2,  SQ_A4 = 3,  SQ_A5 = 4,  SQ_A6 = 5,  SQ_A7 = 6,  SQ_A8 = 7,
-        SQ_B1 = 8,  SQ_B2 = 9,  SQ_B3 = 10, SQ_B4 = 11, SQ_B5 = 12, SQ_B6 = 13, SQ_B7 = 14, SQ_B8 = 15,
-        SQ_C1 = 16, SQ_C2 = 17, SQ_C3 = 18, SQ_C4 = 19, SQ_C5 = 20, SQ_C6 = 21, SQ_C7 = 22, SQ_C8 = 23,
-        SQ_D1 = 24, SQ_D2 = 25, SQ_D3 = 26, SQ_D4 = 27, SQ_D5 = 28, SQ_D6 = 29, SQ_D7 = 30, SQ_D8 = 31,
-        SQ_E1 = 32, SQ_E2 = 33, SQ_E3 = 34, SQ_E4 = 35, SQ_E5 = 36, SQ_E6 = 37, SQ_E7 = 38, SQ_E8 = 39,
-        SQ_F1 = 40, SQ_F2 = 41, SQ_F3 = 42, SQ_F4 = 43, SQ_F5 = 44, SQ_F6 = 45, SQ_F7 = 46, SQ_F8 = 47,
-        SQ_G1 = 48, SQ_G2 = 49, SQ_G3 = 50, SQ_G4 = 51, SQ_G5 = 52, SQ_G6 = 53, SQ_G7 = 54, SQ_G8 = 55,
-        SQ_H1 = 56, SQ_H2 = 57, SQ_H3 = 58, SQ_H4 = 59, SQ_H5 = 60, SQ_H6 = 61, SQ_H7 = 62, SQ_H8 = 63,
+        SQ_A1 = 0,  SQ_B1 = 1,  SQ_C1 = 2,  SQ_D1 = 3,  SQ_E1 = 4,  SQ_F1 = 5,  SQ_G1 = 6,  SQ_H1 = 7,
+        SQ_A2 = 8,  SQ_B2 = 9,  SQ_C2 = 10, SQ_D2 = 11, SQ_E2 = 12, SQ_F2 = 13, SQ_G2 = 14, SQ_H2 = 15,
+        SQ_A3 = 16, SQ_B3 = 17, SQ_C3 = 18, SQ_D3 = 19, SQ_E3 = 20, SQ_F3 = 21, SQ_G3 = 22, SQ_H3 = 23,
+        SQ_A4 = 24, SQ_B4 = 25, SQ_C4 = 26, SQ_D4 = 27, SQ_E4 = 28, SQ_F4 = 29, SQ_G4 = 30, SQ_H4 = 31,
+        SQ_A5 = 32, SQ_B5 = 33, SQ_C5 = 34, SQ_D4 = 35, SQ_E5 = 36, SQ_F5 = 37, SQ_G5 = 38, SQ_H5 = 39,
+        SQ_A6 = 40, SQ_B6 = 41, SQ_C6 = 42, SQ_D6 = 43, SQ_E6 = 44, SQ_F6 = 45, SQ_G6 = 46, SQ_H6 = 47,
+        SQ_A7 = 48, SQ_B7 = 49, SQ_C7 = 50, SQ_D7 = 51, SQ_E7 = 52, SQ_F7 = 53, SQ_G7 = 54, SQ_H7 = 55,
+        SQ_A8 = 56, SQ_B8 = 57, SQ_C8 = 58, SQ_D8 = 59, SQ_E8 = 60, SQ_F8 = 61, SQ_G8 = 62, SQ_H8 = 63,
+        SQ_NONE = 64
 };
-
-//0000000000001000000000000000000000000000000000000000000000000000
-//0000000000000000000000000000000000000000000000000000000000000000
-//0000000000001000000000000000000000000000000000000000000000000000 - the result
 
 
 /*  HELPER FUNCTIONS  */
